741eb3ead1af36014bfed6634d69a66a4fe7f0d9
acwcheung <acwcheung1@gmail.com>
2020-02-12 23:24:52 +0800

    Updates

---
+++ precache-manifest.b7adb8631be1936a28021341c1ed3550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee58a53fcbb2bcf34b86fbc203108d4",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "eb4c64db6d68ba61cac4",
+    "url": "/react-app/static/css/main.ae40fd74.chunk.css"
+  },
+  {
+    "revision": "180ce2631089d00bc19a",
+    "url": "/react-app/static/js/2.df2afca4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.df2afca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb4c64db6d68ba61cac4",
+    "url": "/react-app/static/js/main.5d8381bb.chunk.js"
+  },
+  {
+    "revision": "051414516e198db06575",
+    "url": "/react-app/static/js/runtime-main.1aaed6ec.js"
+  },
+  {
+    "revision": "48c4b69aa68a05e84f62b89a2c9e45ea",
+    "url": "/react-app/static/media/stone.48c4b69a.jpg"
+  }
+]);
